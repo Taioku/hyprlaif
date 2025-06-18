2804eb53a27164fd996be06de56b0ed0ba4a2ff8
taioku <t.rgpns@gmail.com>
2025-06-18 10:29:01 +0000

    fixed scripts

--- .config/hypr/scripts/authorize_files.sh
+++ .config/hypr/scripts/authorize_files.sh
@@ -3,4 +3,3 @@
 cfg_path="$HOME/.config"
 
 chmod +x "$cfg_path/waybar/scripts/refresh.sh"
-chmod +x "$cfg_path/scripts/re-sync_sysclock.sh"
--- .config/scripts/re-sync_sysclock.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo timedatectl set-ntp false 
-sudo timedatectl set-ntp true
